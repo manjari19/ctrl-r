a2d114f06d4ddd42454a40e0c3e38b6479a7aa96
Aeoli11 <writeaeris@gmail.com>
2026-01-31 19:48:37 -0800

    Update filedescs.js

---
+++ ctrl-r/client/src/backend/filedescs.js
@@ -0,0 +1,11 @@
+export const File_Labels = {
+    bmp:"Bitmap(.bmp)",
+    csv:"Comma Separated Values(.csv)",
+
+
+
+}
+
+export const File_Desc={
+    bmp:"An image format devoloped by Microsoft-format used specfically for Windows applications.",
+}
